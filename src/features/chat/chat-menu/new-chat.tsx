7bad9371b15675edf7c0d83f802dd5e934a5fd0c
Megumiomr <megumi-omuro@toyal.co.jp>
2024-10-01 20:39:28 +0900

    Add files via upload

--- src/features/chat/chat-menu/new-chat.tsx
+++ src/features/chat/chat-menu/new-chat.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import { PlusCircle } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { CreateChatThread } from "../chat-services/chat-thread-service";
+import { ChatMessageEmptyState } from "@/features/chat/chat-ui/chat-empty-state/chat-message-empty-state";
 
 export const NewChat = () => {
   const router = useRouter();
@@ -20,6 +21,7 @@ export const NewChat = () => {
   };
 
   return (
+    <div>
     <Button
       className="gap-2 rounded-full w-[40px] h-[40px] p-1 text-primary"
       variant={"outline"}
@@ -27,5 +29,19 @@ export const NewChat = () => {
     >
       <PlusCircle size={40} strokeWidth={1.2} />
     </Button>
+    <Button
+      className="gap-2 rounded-full w-[40px] h-[40px] p-1 text-primary"
+      variant={"outline"}
+      onClick={() => <ChatMessageEmptyState />}
+    >
+      <PlusCircle size={40} strokeWidth={1.2} />
+    </Button>
+    </div>
+
+
+
+
+
+    
   );
 };
