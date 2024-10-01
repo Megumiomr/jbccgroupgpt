82f6eb471cfdfbe0db7c434f365ee5fa6cd588c1
Megumiomr <megumi-omuro@toyal.co.jp>
2024-10-01 17:19:54 +0900

    Add files via upload

--- src/features/main-menu/menu.tsx
+++ src/features/main-menu/menu.tsx
@@ -52,34 +52,24 @@ export const MainMenu = () => {
             className="rounded-full w-[40px] h-[40px] p-2 text-primary"
             variant={"outline"}
           >
-            <Link href="/reporting" title="管理者向けレポート">
-              <LayoutDashboard />
+            <Link href="/option" title="設定">
+            <Sticker />
             </Link>
           </Button>
         ) : (
           <></>
         )}
-        <Button
-          asChild
-          className="rounded-full w-[40px] h-[40px] p-2 text-primary"
-          variant={"outline"}
-        >
-          <Link target="_blank" href='https://prompt.quel.jp/index.php?imode=1&theme=%E3%83%93%E3%82%B8%E3%83%8D%E3%82%B9' title="便利な使い方" className="relative">
-            <Lightbulb />
-            <UpdateIndicator />
-          </Link>
-        </Button>
-        {session?.user?.isAdmin ? (
 
-        <Button
-           asChild
-           className="rounded-full w-[40px] h-[40px] p-2 text-primary"
-           variant={"outline"}
-         >
-           <Link href="/chat" title="社内問い合わせ">
-             <Sticker />
-           </Link>       
-        </Button>
+        {session?.user?.isAdmin ? (
+          <Button
+            asChild
+            className="rounded-full w-[40px] h-[40px] p-2 text-primary"
+            variant={"outline"}
+          >
+            <Link href="/reporting" title="レポート">
+              <LayoutDashboard />
+            </Link>
+          </Button>
         ) : (
           <></>
         )}
