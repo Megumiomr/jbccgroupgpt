9cd2350f9b9b1a1503f5bf23f323d040739bdc23
Megumiomr <megumi-omuro@toyal.co.jp>
2024-10-01 17:14:33 +0900

    Add files via upload

--- src/features/chat/chat-ui/chat-ui.tsx
+++ src/features/chat/chat-ui/chat-ui.tsx
@@ -2,7 +2,6 @@
 
 import { FC } from "react";
 import { useChatContext } from "./chat-context";
-import { ChatMessageEmptyState } from "./chat-empty-state/chat-message-empty-state";
 import ChatInput from "./chat-input/chat-input";
 import { ChatMessageContainer } from "./chat-message-container";
 
@@ -13,13 +12,10 @@ export const ChatUI: FC<Prop> = () => {
 
   return (
     <div className="h-full relative overflow-hidden flex-1 bg-card rounded-md shadow-md">
-      {messages.length !== 0 ? (
-        <ChatMessageContainer />
-      ) : (
-        <ChatMessageEmptyState />
-      )}
 
+      <ChatMessageContainer />
       <ChatInput />
+    
     </div>
   );
 };
