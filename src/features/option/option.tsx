00a7e06cfea87221036b924d2066ebf4922c3cdd
Megumiomr <megumi-omuro@toyal.co.jp>
2024-10-07 13:30:39 +0900

    Add files via upload

--- src/features/option/option.tsx
+++ src/features/option/option.tsx
@@ -8,10 +8,18 @@ interface Prop {}
 export const Option = () => {
 
   return (
-    <div className="h-full relative overflow-hidden flex-1 bg-card rounded-md shadow-md">
-   
-    <OptionFileUI />
+    <div className="grid grid-cols-1 w-full items-center container mx-auto max-w-3xl justify-center h-full gap-9">
+      <Card className="col-span-3 flex flex-col gap-5 p-5 ">
+        <Typography variant="h4" className="text-primary">
+          ファイル読込
+        </Typography>
 
+        <div className="flex flex-col gap-2">
+          <OptionFileUI />
+        </div>
+
+        
+      </Card>
     </div>
   );
 };
