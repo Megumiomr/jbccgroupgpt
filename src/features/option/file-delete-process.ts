01c83925f8dc1e87010a7172049f9388c60e9254
Megumiomr <megumi-omuro@toyal.co.jp>
2024-10-24 17:14:27 +0900

    Delete src/features/option/file-delete-process.ts

--- src/features/option/file-delete-process.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import { deleteAllDocuments } from "@/features/chat/chat-services/azure-cog-search/azure-cog-vector-store";
-
-export const FileDeleteProcess = async () => {
-
-    await deleteAllDocuments();
-
-};
