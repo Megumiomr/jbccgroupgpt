26be8e1ff67566d4b784885f2f77d80b13d3391f
Megumiomr <megumi-omuro@toyal.co.jp>
2024-10-24 15:56:09 +0900

    Add files via upload

--- src/features/option/file-delete-process.ts
+++ src/features/option/file-delete-process.ts
@@ -1,9 +1,16 @@
 "use server";
 
+import { useGlobalMessageContext } from "@/features/global-message/global-message-context";
 import { deleteAllDocuments } from "@/features/chat/chat-services/azure-cog-search/azure-cog-vector-store";
 
 export const FileDeleteProcess = async () => {
 
-    await deleteAllDocuments();
+    const { showError } = useGlobalMessageContext();
+
+    try {
+        await deleteAllDocuments();
+    } catch (e) {
+        showError("" + e);
+    }
 
 };
